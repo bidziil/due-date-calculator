8882f87ae53582c962cce62d70e4415664fe1ef2
MokusKocka <MokusKocka@DESKTOP-5VQP1F2>
2019-11-22 23:34:26 +0100

    setup a parameterized unit test with hour, day, month and year change

--- src/main/java/com/emarsys/so/ho/DueDateCalculator.java
+++ src/main/java/com/emarsys/so/ho/DueDateCalculator.java
@@ -1,12 +1,22 @@
 package com.emarsys.so.ho;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.EnumSet;
+import java.util.logging.Logger;
 
 public class DueDateCalculator {
+	private static final Logger	LOGGER	= Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	private static final EnumSet<DayOfWeek> WEEKEND_DAYS = EnumSet.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
 
+	/**
+	 * @param submitDateTime
+	 * @param turnaroundTime
+	 * @return the date/time when the issue is resolved
+	 **/
 	public LocalDateTime calculateDueDate(final LocalDateTime submitDateTime, final Duration turnaroundTime) {
-		return null;
+		LOGGER.info(String.format("calculateDueDate start submitDateTime:%s turnaroundTime:%s", submitDateTime, turnaroundTime));
+		return submitDateTime;
 	}
-
 }
--- src/test/java/com/emarsys/so/ho/DueDateCalculatorTest.java
+++ src/test/java/com/emarsys/so/ho/DueDateCalculatorTest.java
@@ -1,14 +1,20 @@
 package com.emarsys.so.ho;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.logging.Logger;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class DueDateCalculatorTest {
 	private static final Logger	LOGGER	= Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
@@ -25,18 +31,54 @@ class DueDateCalculatorTest {
 	void setUp() throws Exception {
 	}
 
-	@Test
-	void testCalculateDueDate() {
+	public static Collection<Object[]> parameters() {
+		Collection<Object[]> parameters = new ArrayList<Object[]>();
+		
+		parameters.add(new Object[] { LocalDateTime.of(2019, 06, 06,  9, 59), null,                LocalDateTime.of(2019, 06, 06,  9, 59) });
+		parameters.add(new Object[] { LocalDateTime.of(2019, 06, 06,  9, 59), Duration.ofHours(0), LocalDateTime.of(2019, 05, 05,  9, 59) });
+		parameters.add(new Object[] { LocalDateTime.of(2019, 06, 06,  9, 59), Duration.ofHours(2), LocalDateTime.of(2019, 06, 06, 11, 59) });
+		parameters.add(new Object[] { LocalDateTime.of(2019, 06, 06, 16, 59), Duration.ofHours(1), LocalDateTime.of(2019, 06, 06,  9, 59) });
+		parameters.add(new Object[] { LocalDateTime.of(2019, 06, 30, 16, 59), Duration.ofHours(1), LocalDateTime.of(2019, 07, 01,  9, 59) });
+		parameters.add(new Object[] { LocalDateTime.of(2019, 12, 31, 16, 59), Duration.ofHours(0), LocalDateTime.of(2020, 01, 05,  9, 59) });
+		
+		return parameters;
+	}
+
+	@ParameterizedTest
+	@MethodSource("parameters")
+	void testCalculateDueDate(LocalDateTime submitDateTime, Duration turnaroundTime, LocalDateTime expectedResult) {
 		// Given:
-		LocalDateTime submitDateTime = LocalDateTime.now();
-		Duration turnaroundTime = Duration.ofHours(10);
-		LOGGER.fine("testCalculateDueDate start");
+		LOGGER.fine(String.format("testCalculateDueDate start %s %s", submitDateTime, turnaroundTime));
 		
 		// When:
 		LocalDateTime result = dueDateCalculator.calculateDueDate(submitDateTime, turnaroundTime);
 		
 		// Then:
 		assertNotNull(result);
+		assertEquals(expectedResult, result);
+	}
+
+	public static Collection<Object[]> parameters_WrongWorkingHours() {
+		Collection<Object[]> parameters = new ArrayList<Object[]>();
+		
+		parameters.add(new Object[] { null, null });
+		parameters.add(new Object[] { LocalDateTime.of(2019, 05, 05, 8, 59), null });
+		parameters.add(new Object[] { LocalDateTime.of(2019, 05, 05, 17, 59), null });
+		
+		return parameters;
+	}
+	
+	@ParameterizedTest
+	@MethodSource("parameters_WrongWorkingHours")
+	void testCalculateDueDate_WithWrongWorkingHours(LocalDateTime submitDateTime, Duration turnaroundTime) {
+		// Given:
+		LOGGER.fine(String.format("testCalculateDueDate_WithWrongWorkingHours start %s %s", submitDateTime, turnaroundTime));
+		
+		// When:
+		Executable executable = () -> dueDateCalculator.calculateDueDate(submitDateTime, turnaroundTime);
+		
+		// Then:
+		assertThrows(IllegalArgumentException.class, executable);
 	}
 
 }
