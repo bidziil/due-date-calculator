1f11709020b93ff8eaa5933ecaf4b288b7724518
MokusKocka <MokusKocka@DESKTOP-5VQP1F2>
2019-11-22 22:31:50 +0100

    setup basic unit test

--- src/main/java/com/emarsys/so/ho/DueDateCalculator.java
+++ src/main/java/com/emarsys/so/ho/DueDateCalculator.java
@@ -1,8 +1,12 @@
 package com.emarsys.so.ho;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class DueDateCalculator {
 
-	public void calculateDueDate() {
+	public LocalDateTime calculateDueDate(final LocalDateTime submitDateTime, final Duration turnaroundTime) {
+		return null;
 	}
 
 }
--- src/test/java/com/emarsys/so/ho/DueDateCalculatorTest.java
+++ src/test/java/com/emarsys/so/ho/DueDateCalculatorTest.java
@@ -1,14 +1,42 @@
 package com.emarsys.so.ho;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DueDateCalculatorTest {
+	private static final Logger	LOGGER	= Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+	static DueDateCalculator dueDateCalculator;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		LoggerInitializer.initRootLogger();
+		dueDateCalculator = new DueDateCalculator();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
 
 	@Test
 	void testCalculateDueDate() {
-		fail("Not yet implemented");
+		// Given:
+		LocalDateTime submitDateTime = LocalDateTime.now();
+		Duration turnaroundTime = Duration.ofHours(10);
+		LOGGER.fine("testCalculateDueDate start");
+		
+		// When:
+		LocalDateTime result = dueDateCalculator.calculateDueDate(submitDateTime, turnaroundTime);
+		
+		// Then:
+		assertNotNull(result);
 	}
 
 }
